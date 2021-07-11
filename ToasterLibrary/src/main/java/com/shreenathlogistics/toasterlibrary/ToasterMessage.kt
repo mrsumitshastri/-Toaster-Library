60d9397469fe6bb86d87db226f2cb09f8b930685
Sumit Shastri <kshastri@in.ibm.com>
2021-07-11 15:37:11 +0530

    Inital

---
+++ ToasterLibrary/src/main/java/com/shreenathlogistics/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,11 @@
+package com.shreenathlogistics.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+public class ToasterMessage {
+
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
